31e460ec3a8199687597223774180b278bc53240
carlo-baron <carlobaron430@gmail.com>
2024-09-29 17:19:21 +0800

    Character Movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Vector2 moveInput;
+    public float moveSpeed;
+    Rigidbody2D rb;
+
+    void Awake(){
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+    void FixedUpdate()
+    {
+        moveInput.x = Input.GetAxisRaw("Horizontal");   
+        moveInput.y = Input.GetAxisRaw("Vertical");
+
+        Vector2 newVelocity = new Vector2(moveInput.x, moveInput.y);
+        rb.velocity = newVelocity.normalized * moveSpeed;
+    }
+}
