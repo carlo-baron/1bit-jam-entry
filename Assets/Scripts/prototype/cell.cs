c04e674ffdb6813a05b20f403f3efa2583d76ea0
carlo-baron <carlobaron430@gmail.com>
2024-09-29 16:36:41 +0800

    Falling tile logic
    followed a youtube tutorial for this one

---
+++ Assets/Scripts/prototype/cell.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cell : MonoBehaviour
+{
+    int camSize = 1000;
+    int cellSize = 100;
+    public int[,] grid;
+    GameObject[,] objGrid;
+    int rows;
+    int cols;
+
+
+    public GameObject Tile;
+    void Start()
+    {
+        rows = camSize / cellSize;
+        cols = camSize / cellSize;
+        grid = makeGrid(rows, cols);
+        objGrid = new GameObject[rows, cols];
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                grid[i, j] = 0;
+                Vector3 newPos = new Vector2(i, j);
+                objGrid[i, j] = Instantiate(Tile, transform.position + newPos, Quaternion.identity, transform);
+            }
+        }
+
+        StartCoroutine(DelayedUpdate(1));
+    }
+
+    private IEnumerator DelayedUpdate(int delay)
+    {
+        while (true)
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Color temp = objGrid[i, j].GetComponent<SpriteRenderer>().color;
+                    temp.a = grid[i, j];
+                    objGrid[i, j].GetComponent<SpriteRenderer>().color = temp;
+                }
+            }
+
+            int[,] nextGrid = makeGrid(rows, cols);
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    int currentState = grid[i, j];
+                    if (currentState == 1)
+                    {
+                        if (j > 0)
+                        {
+                            int below = grid[i, j - 1];
+                            if (below == 0)
+                            {
+                                nextGrid[i, j - 1] = 1;
+                            }
+                            else
+                            {
+                                nextGrid[i, j] = 1;
+                            }
+                        }
+                        else
+                        {
+                            nextGrid[i, j] = 1;
+                        }
+                    }
+                }
+            }
+            grid = nextGrid;
+            yield return new WaitForSeconds(delay);
+        }
+    }
+
+    int[,] makeGrid(int rows, int cols)
+    {
+        int[,] array = new int[rows, cols];
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                array[i, j] = 0;
+            }
+        }
+
+        return array;
+    }
+}
--- Assets/Scripts/tileBehavior.cs
+++ Assets/Scripts/tileBehavior.cs
@@ -6,45 +6,12 @@ using UnityEngine;
 
 public class tileBehavior : MonoBehaviour
 {
-    public float gravity = 9.81f;
-    private Vector2 velocity;
-    private bool isFalling = true;
+   cell cellScript;
 
-    void Update()
-    {
-        if (isFalling)
-        {
-            velocity.y -= gravity * Time.deltaTime;
-
-            transform.position += (Vector3)velocity * Time.deltaTime;
-
-            if (IsGrounded())
-            {
-                isFalling = false;
-                velocity = Vector2.zero;
-                // SnapToGround();
-            }
-        }else if(!IsGrounded()){
-            isFalling = true;
-        }
-
-        
+    void Awake(){
+        cellScript = transform.parent.GetComponent<cell>();
     }
-
-    private bool IsGrounded()
-    {
-        RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - 0.5f), Vector2.down, 0.1f);
-        return hit.collider != null;
-    }
-
-    private void SnapToGround()
-    {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, Mathf.Infinity);
-        if (hit.collider != null)
-        {
-            transform.position = new Vector3(transform.position.x, hit.point.y + 0.5f, transform.position.z);
-            isFalling = false;
-            velocity = Vector2.zero;
-        }
+    void OnMouseDown(){
+        cellScript.grid[(int)transform.localPosition.x, (int)transform.localPosition.y] = 1;
     }
 }
