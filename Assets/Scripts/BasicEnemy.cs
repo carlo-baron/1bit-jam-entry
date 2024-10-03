a4e39ff21694663afd2020fb4af4e35df6b10089
carlo-baron <carlobaron430@gmail.com>
2024-10-03 16:07:53 +0800

    Enemies(kinds)

--- Assets/Scripts/BasicEnemy.cs
+++ Assets/Scripts/BasicEnemy.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TriangleEnemy : Enemy, IDamageable
+public class BasicEnemy : Enemy, IDamageable
 {
     [SerializeField] int Health;
     [SerializeField] float Speed;
