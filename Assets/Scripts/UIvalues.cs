97b22c868331fa064386ac693654fdaf6ef69522
carlo-baron <carlobaron430@gmail.com>
2024-10-03 18:22:30 +0800

    Mine logic

--- Assets/Scripts/UIvalues.cs
+++ Assets/Scripts/UIvalues.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIvalues : MonoBehaviour
 {
@@ -7,9 +8,24 @@ public class UIvalues : MonoBehaviour
 
     [SerializeField] TextMeshProUGUI HP;
     [SerializeField] TextMeshProUGUI Mine;
+    [SerializeField] GameObject gameOver;
+
+    void Awake(){
+        Time.timeScale = 1;
+    }
+
     void Update()
     {
         HP.text = $"HP: {player.PlayerHealth}";
         Mine.text = $"HP: {player.mine}";
+
+        if(player.PlayerHealth <= 0){
+            gameOver.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+
+    public void Restart(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
--- Assets/Scripts/tileBehavior.cs
+++ Assets/Scripts/tileBehavior.cs
@@ -3,15 +3,20 @@ using UnityEngine;
 public class tileBehavior : MonoBehaviour
 {
     CellularAutomata cellularAutomata;
+    PlayerMovement player;
 
     void Awake()
     {
         cellularAutomata = transform.parent.GetComponent<CellularAutomata>();
+        player = GameObject.FindGameObjectWithTag("player").GetComponent<PlayerMovement>();
     }
     void OnMouseOver()
     {
-        if(Input.GetMouseButtonDown(1)){
-            cellularAutomata.grid[(int)transform.localPosition.x, (int)transform.localPosition.y] = 0;
+        if(player.mine > 0){
+            if(Input.GetMouseButtonDown(1)){
+                cellularAutomata.grid[(int)transform.localPosition.x, (int)transform.localPosition.y] = 0;
+                player.mine--;
+            }
         }
     }
 }
